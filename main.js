3cca997bcc21ab43443c6205feceb10d6fd44af1
Sung Paik <sung.paik@codeimmersives.com>
2019-12-14 14:04:17 -0500

    included event listeners

---
+++ main.js
@@ -0,0 +1,5 @@
+const pomodoroTimer = document.querySelector('#pomodoro-timer');
+const startButton = document.querySelector('#pomodoro-start');
+const pauseButton = document.querySelector('#pomodoro-pause');
+const stopButton = document.querySelector('#pomodoro-stop');
+
