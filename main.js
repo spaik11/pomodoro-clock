ed2fcad457aceff2358bb05378573b12ae97b66b
Sung Paik <sung.paik@codeimmersives.com>
2019-12-14 14:18:29 -0500

    create variables for the timer in seconds

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const stopButton = document.querySelector('#stop');
 
+let isClockRunning = false;
+let workSeshDuration = 1500;
+let timeLeftInSession = 1500;
+let breakSeshDuration = 300;
+
 startButton.addEventListener('click', () => {
 
 })
